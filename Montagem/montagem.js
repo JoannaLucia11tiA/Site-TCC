391a64b6009b00ae5f7b17bc90f9f2d891b0f9b1
Nickolas-Zilig-Oliveira <joannalucia25@gmail.com>
2026-08-30 17:22:32 -0300

    Mobile da pg 2 qse finalizado

---
+++ Montagem/montagem.js
@@ -0,0 +1,23 @@
+ function abrirMenu() {
+        document.getElementById("menuMobile").classList.add("aberto")
+    }
+
+    function fecharMenu() {
+        document.getElementById("menuMobile").classList.remove("aberto")
+    }
+
+function temaClaro() {
+    document.body.classList.remove("escuro");
+
+    document.querySelectorAll(".tema").forEach(tema => {
+        tema.classList.remove("escuro");
+    });
+}
+
+function temaEscuro() {
+    document.body.classList.add("escuro");
+
+    document.querySelectorAll(".tema").forEach(tema => {
+        tema.classList.add("escuro");
+    });
+}
